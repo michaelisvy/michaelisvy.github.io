e695d70a6d4652bef3f9b6f5b8aadcdfa9ac46c3
Michael Isvy <michael.isvy@gmail.com>
2023-07-05 23:40:32 +0800

    troubleshooting

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/michaelisvy.github.io/',
+  base: './',
   plugins: [react()],
 
   build: {
